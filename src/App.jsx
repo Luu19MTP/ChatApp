9660f2683f7fab4155d4a79fa1dcd7ed45a0bea7
Luu <luu@gmail.com>
2024-07-04 11:18:14 +0700

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import LoginForm from './components/LoginForm/LoginForm'
 
 
 
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>do an cuoi ki</h1>
+      <LoginForm></LoginForm>
     </>
   )
 }
